06c4d1e958d437450f02cdabd9cde4e9b3d2abc1
Владислав Трифонов <gr643_tlool@lab-203-3.tomtit-tomsk.ru>
2026-04-17 15:58:06 +0700

    Initial commit: base structure

---
+++ mypett.py
@@ -0,0 +1,9 @@
+def main():
+    print("Добро пожаловать в MyPett!")
+    print("1. Покормить")
+    print("2. Поиграть")
+    print("3. Уход")
+    print("4. Статус")
+
+if __name__ == "__main__":
+    main()
